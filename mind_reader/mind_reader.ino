739637fde2f1153d40fbe444bd6b8a925eb4e4d3
Nathan Telles <me@nathantelles.com>
2017-04-05 16:24:41 -0700

    Fixed code and updated code to control 5 servos

--- mind_reader/mind_reader.ino
+++ mind_reader/mind_reader.ino
@@ -3,17 +3,30 @@
 
 // Set up the brain parser, pass it the hardware serial object you want to listen on.
 Brain brain(Serial);
-Servo servo1;
-float average_attention = 0;
-float average_position = 0;
-float motor_position = 0; # The current motor position
-float increment_amount = 0.15; # Controls speed which motor can move from one position to another
-float buffer = 1; # If the motor is between +buffer or -buffer dont change the position
+const int numberOfServos = 5;
+Servo *servos[numberOfServos];
+float averageAttention = 0;
+float averagePosition = 0;
+float motorPosition = 0; // The current motor position
+float incrementAmount = 0.15; // Controls speed which motor can move from one position to another
+const float buffer = 1; // If the motor is between +buffer or -buffer dont change the position
+
 
 void setup() {
   // Start the hardware serial.
+  int pins[5] = {9, 10, 11, 12, 13};
   Serial.begin(9600);
-  servo1.attach(9);
+  for (int i=0; i < numberOfServos; ++i) {
+    Serial.println(averageAttention);
+    servos[i] = new Servo();
+    servos[i]->attach(pins[i]);
+  }
+}
+
+void updateServoPositions(float position) {
+  for (int i=0; i < numberOfServos; ++i) {
+    servos[i]->write(position);
+  }
 }
 
 void loop() {
@@ -23,27 +36,25 @@ void loop() {
   if (brain.update()) {
     Serial.println(brain.readCSV());
     int attention = brain.readAttention();
-# Take weighted average of attention with more enphasis on newer values
-    average_attention = average_attention * 0.4 + attention * 0.6;
+// Take weighted average of attention with more enphasis on newer values
+    averageAttention = averageAttention * 0.4 + attention * 0.6;
 
-# So far we have seen values between 0 and 100 for
-    average_position = 2 * average_attention;
+// So far we have seen values between 0 and 100 for
+    averagePosition = 2 * averageAttention;
   }
 
-# Incrementaly rotate the motor so that the fingers do not jerk around
-  if (motor_position < average_position - buffer) {
-    motor_position += increment_amount;
-    Serial.println(motor_position);
+// Incrementaly rotate the motor so that the fingers do not jerk around
+  if (motorPosition < averagePosition - buffer) {
+    motorPosition += incrementAmount;
+    Serial.println(motorPosition);
     delay(10);
   }
-  else if (motor_position > average_position + buffer) {
-    motor_position -= increment_amount;
-    Serial.println(motor_position);
+  else if (motorPosition > averagePosition + buffer) {
+    motorPosition -= incrementAmount;
+    Serial.println(motorPosition);
     delay(10);
   }
-  servo1.write(motor_position);
 
-
-
+  updateServoPositions(motorPosition);
 }
 
